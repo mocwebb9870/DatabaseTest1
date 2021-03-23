2ef28d9e90ef020f9fb3265ddff2d620db410282
mocwebb9870 <tu19ni42@yahoo.co.jp>
2021-03-23 21:59:42 +0900

    20210323

---
+++ README.md
@@ -0,0 +1,3 @@
+# DatabaseTest1
+
+original SOURCE https://www.codejava.net/frameworks/spring-boot/spring-boot-crud-example-with-spring-mvc-spring-data-jpa-thymeleaf-hibernate-mysql
---
+++ src/main/java/com/example/demo/DatabaseTest1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseTest1Application.class, args);
+	}
+
+}
